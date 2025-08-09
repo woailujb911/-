a15a2e0394fc229907d16f56621da5f8949527c2
卡卡大帝 <3804046925@qq.com>
2025-08-09 16:42:19 +0800

    Add files via upload

---
+++ 外星人.lua
@@ -0,0 +1,66 @@
+        local Players = game:GetService("Players");
+		local player = Players.LocalPlayer;
+		local username = player.Name;
+		local Sound = Instance.new("Sound", workspace);
+		Sound.SoundId = "rbxassetid://117383430326696";
+		Sound.PlayOnRemove = true;
+		Sound:Destroy();
+		game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text=("欢迎尊贵的卡卡用户: " .. username),Icon="rbxassetid://117383430326696"});
+		wait(1.5);
+		local Sound = Instance.new("Sound", workspace);
+		Sound.SoundId = "rbxassetid://117383430326696";
+		Sound.PlayOnRemove = true;
+		Sound:Destroy();
+		game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="正在启动脚本中..",Icon="rbxassetid://117383430326696"});
+		wait(1.5);
+		if ((game.GameId == 8888615802) or (game.GameId == 13622981808)) then
+			loadstring(game:HttpGet(""))();
+		elseif (game.GameId == 2820580801) then
+			local Sound = Instance.new("Sound", workspace);
+			Sound.SoundId = "rbxassetid://117383430326696";
+			Sound.PlayOnRemove = true;
+			Sound:Destroy();
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="正在打开俄亥俄州脚本",Icon="rbxassetid://117383430326696"});
+			local Library = loadstring(game:HttpGet("https://github.com/eksan966/Alienx-Cracked-version/blob/main/BladeBall.lua"))();
+			local Sound = Instance.new("Sound", workspace);
+			Sound.SoundId = "rbxassetid://117383430326696";
+			Sound.PlayOnRemove = true;
+			Sound:Destroy();
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="俄亥俄州启动成功",Icon="rbxassetid://117383430326696"});
+		elseif (game.GameId == 4777817887) then
+			local Sound = Instance.new("Sound", workspace);
+			Sound.SoundId = "rbxassetid://117383430326696";
+			Sound.PlayOnRemove = true;
+			Sound:Destroy();
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="正在打开Blade Ball脚本",Icon="rbxassetid://117383430326696"});
+			local Library = loadstring(game:HttpGet("https://pastefy.app/dGZvOIoi/raw"))();
+			local Sound = Instance.new("Sound", workspace);
+			Sound.SoundId = "rbxassetid://117383430326696";
+			Sound.PlayOnRemove = true;
+			Sound:Destroy();
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="Blade Ball启动成功",Icon="rbxassetid://117383430326696"});
+		elseif (game.GameId == 1526814825) then
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="正在打开战争大亨脚本",Icon="rbxassetid://117383430326696"});
+			local Library = loadstring(game:HttpGet("https://pastefy.app/9k3Yc3FU/raw"))();
+			local Sound = Instance.new("Sound", workspace);
+			Sound.SoundId = "rbxassetid://117383430326696";
+			Sound.PlayOnRemove = true;
+			Sound:Destroy();
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="战争大亨启动成功",Icon="rbxassetid://117383430326696"});
+               elseif (game.GameId == 1335695570) then
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="正在打开忍者传奇脚本",Icon="rbxassetid://117383430326696"});
+			local Library = loadstring(game:HttpGet("https://pastefy.app/djHdgWm7/raw"))();
+			local Sound = Instance.new("Sound", workspace);
+			Sound.SoundId = "rbxassetid://117383430326696";
+			Sound.PlayOnRemove = true;
+			Sound:Destroy();
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="忍者传奇启动成功",Icon="rbxassetid://117383430326696"});
+               elseif (game.GameId == 7326934954) then
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="正在打开森林中的99夜",Icon="rbxassetid://117383430326696"});
+			local Library = loadstring(game:HttpGet("https://pastefy.app/7IxJgiPl/raw"))();
+			local Sound = Instance.new("Sound", workspace);
+			Sound.SoundId = "rbxassetid://117383430326696";
+			Sound.PlayOnRemove = true;
+			Sound:Destroy();
+			game.StarterGui:SetCore("SendNotification", {Title="卡卡",Text="森林中的99夜启动成功",Icon="rbxassetid://117383430326696"});
+		end
